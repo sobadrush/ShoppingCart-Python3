517f11e6ef0826f6f2192790f31df2907d61afb7
sobadrush <sobadrush@icloud.com>
2020-07-15 01:16:07 +0800

    自定義管理介面顯示的model屬性

--- Book/admin.py
+++ Book/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-
 from Book.models import BookInfo, RoleInfo
 
 # Register your models here.
+
+class RoleIndoAdmin(admin.ModelAdmin):
+    """角色model後臺管理介面的管理類別"""
+    list_display = ['id', 'rName', 'rGender', 'book']
 
 # 註冊BookInfo
 admin.site.register(BookInfo)
 
 # 註冊RoleInfo
-admin.site.register(RoleInfo)
+admin.site.register(RoleInfo, RoleIndoAdmin)
