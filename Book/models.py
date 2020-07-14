191a4235f954d0df692e0a2b1e5b2e5fe6cf253a
sobadrush <sobadrush@icloud.com>
2020-07-15 00:28:52 +0800

    完成MSSQL建立Book及Role及TEST表migrate

--- Book/models.py
+++ Book/models.py
@@ -17,13 +17,13 @@ class RoleInfo(models.Model):
     book = models.ForeignKey(BoolInfo, on_delete=models.PROTECT) # [on_delete] ref. https://www.coder.work/article/3185936
 
 
-# """ 【建立測試TABLE測試連線】ref. https://www.itread01.com/content/1544108421.html """
-# class TEST(models.Model):
-#     ID = models.IntegerField(primary_key=True,)
-#     NAME = models.CharField(max_length=255,)
+""" 【建立測試TABLE測試連線】ref. https://www.itread01.com/content/1544108421.html """
+class TEST(models.Model):
+    ID = models.IntegerField(primary_key=True,)
+    NAME = models.CharField(max_length=255,)
 
-#     class Meta:	# 如果讀取已有資料的必要引數！
-#         db_table = "Test_Table"
+    class Meta:	# 如果讀取已有資料的必要引數！
+        db_table = "Test_Table"
         
-#     def __str__(self):
-#         return self.NAME
+    def __str__(self):
+        return self.NAME
