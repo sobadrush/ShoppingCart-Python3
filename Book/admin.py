9e2e13a48db49bc61494d4bef4c3eb8c532ea3ec
sobadrush <sobadrush@icloud.com>
2020-07-15 01:09:12 +0800

    透過ADMIN介面建立資料

--- Book/admin.py
+++ Book/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from Book.models import BookInfo, RoleInfo
+
 # Register your models here.
+
+# 註冊BookInfo
+admin.site.register(BookInfo)
+
+# 註冊RoleInfo
+admin.site.register(RoleInfo)
--- Book/models.py
+++ Book/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class BoolInfo(models.Model): # 定義BookInfo並繼承 models.Model
+class BookInfo(models.Model): # 定義BookInfo並繼承 models.Model
     """ 定義書籍Model """
 
     # define attribute
     bName = models.CharField(max_length=10)
+
+
 
 class RoleInfo(models.Model):
     """ 定義角色色Model """
@@ -14,7 +16,11 @@ class RoleInfo(models.Model):
     # define attribute
     rName = models.CharField(max_length=10)
     rGender = models.BooleanField()
-    book = models.ForeignKey(BoolInfo, on_delete=models.PROTECT) # [on_delete] ref. https://www.coder.work/article/3185936
+    book = models.ForeignKey(BookInfo, on_delete=models.PROTECT) # [on_delete] ref. https://www.coder.work/article/3185936
+
+    def __str__(self):
+        """以字串輸出model"""
+        return self.rName
 
 
 """ 【建立測試TABLE測試連線】ref. https://www.itread01.com/content/1544108421.html """
